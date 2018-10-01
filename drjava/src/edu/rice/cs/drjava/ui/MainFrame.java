0410892b93befcd9b7690f54a25392104add2341
Chris Kauffman <kauffman77@gmail.com>
2018-10-01 14:56:30 -0500

    Bug fix for Mac OSX starting minimized.

--- drjava/src/edu/rice/cs/drjava/ui/MainFrame.java
+++ drjava/src/edu/rice/cs/drjava/ui/MainFrame.java
@@ -7871,6 +7871,7 @@ public class MainFrame extends SwingFrame implements ClipboardOwner, DropTargetL
     EventQueue.invokeLater(new Runnable() { 
       public void run() { 
         setVisible(true);
+        setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH); // start large, fixes Mac OSX minimization start problem
         _compilerErrorPanel.setVisible(true);
         showTab(_compilerErrorPanel, true); 
         /* The following two step sequence was laboriously developed by trial and error; without it the _tabbedPane
